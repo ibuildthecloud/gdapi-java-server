70a0916af2eaa5aa008cf08c97ab6011bbb5698f
Darren Shepherd <darren.s.shepherd@gmail.com>
2014-06-19 09:30:17 -0700

    Add attributes to Action object

--- src/main/java/io/github/ibuildthecloud/gdapi/model/Action.java
+++ src/main/java/io/github/ibuildthecloud/gdapi/model/Action.java
@@ -1,20 +1,31 @@
 package io.github.ibuildthecloud.gdapi.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.bind.annotation.XmlTransient;
+
 public class Action {
 
     String input, output;
+    Map<String,Object> attributes = new HashMap<String, Object>();
 
     public Action() {
     }
 
     public Action(Action other) {
-        this(other.getInput(), other.getOutput());
+        this(other.getInput(), other.getOutput(), other.getAttributes());
     }
 
     public Action(String input, String output) {
+        this(input, output, new HashMap<String,Object>());
+    }
+
+    public Action(String input, String output, Map<String,Object> attributes) {
         super();
         this.input = input;
         this.output = output;
+        this.attributes = attributes;
     }
 
     public String getInput() {
@@ -31,6 +42,17 @@ public class Action {
 
     public void setOutput(String output) {
         this.output = output;
+    }
+
+    @XmlTransient
+    @io.github.ibuildthecloud.gdapi.annotation.Field(include = false)
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @XmlTransient
+    public void setAttributes(Map<String, Object> attributes) {
+        this.attributes = attributes;
     }
 
 }
